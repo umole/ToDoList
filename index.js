656f74ae83ec0d30a5d96ad0a39ecaf72e0a2e3b
Umole Andrew <39883080+umole@users.noreply.github.com>
2023-09-10 15:48:59 +0100

    stroking and deleting

--- index.js
+++ index.js
@@ -18,3 +18,12 @@ function addTask() {
     }
     inputBox.value = "";
 }
+
+//Adding JS for the click function
+listContainer.addEventListener("click", function(e){
+    if(e.target.tagName === "LI") {
+        e.target.classList.toggle("checked");
+    } else if(e.target.tagName === "SPAN"){
+        e.target.parentElement.remove();
+    }
+});
